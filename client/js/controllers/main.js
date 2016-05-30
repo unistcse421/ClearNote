2b543c753ba44b6be22f213fe015a315727d890c
이종윤 <jongyun.lee94@gmail.com>
2016-05-30 22:44:52 +0900

    Navbar Styling

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,49 +1,54 @@
-angular.module('cleannote').controller('MainCtrl', ['$scope', 'sections', 'cards', 'todos', function($scope, sections, cards, todos) {
-    $scope.sections = sections.sections;
-    $scope.cards = cards.cards;
-    $scope.todos = todos.todos;
-    $scope.addSection = function() {
-	if(!$scope.course_id || $scope.course_id === '') { return; }
-	if(!$scope.section_id || $scope.section_id === '') { return; }
-	if(!$scope.semester || $scope.semester === '') { return; }
-	if(!$scope.year || $scope.year === '') { return; }
-	if(!$scope.course_name || $scope.course_name === '') { return; }
-	sections.create({
-	    course_id: $scope.course_id,
-	    section_id: $scope.section_id,
-	    semester: $scope.semester,
-	    year: $scope.year,
-	    course_name: $scope.course_name
-	});
-	$scope.course_id = '';
-	$scope.section_id = '';
-	$scope.semester = '';
-	$scope.year = '';
-	$scope.course_name = '';
-    };
-    $scope.addCards = function() {
-	if (!$scope.card_title || $scope.card_title === '') {return;}
-	if (!$scope.card_content || $scope.card_content === '') {return;}
-	cards.create({
-	    card_type: $scope.card_type,
-	    title: $scope.card_title,
-	    content: $scope.card_content,
-	});
-	$scope.card_type = '';
-	$scope.card_title = '';
-	$scope.card_content = '';
-    };
-    $scope.addTodo = function() {
-	if (!$scope.todo_task || $scope.todo_task === '') { return; }
-	todos.create({
-	    task: $scope.todo_task
-	});
-	$scope.todo_task = '';
-    };
-    $scope.finishTask = function(todo) {
-	todos.finish(todo);
-    };
-    $scope.undoTask = function(todo) {
-	todos.undo(todo);
-    };
-}]);
+angular.module('cleannote').controller('MainCtrl', [
+    '$scope',
+    'sections',
+    'cards',
+    'todos',
+    function($scope, sections, cards, todos) {
+	$scope.sections = sections.sections;
+	$scope.cards = cards.cards;
+	$scope.todos = todos.todos;
+	$scope.addSection = function() {
+	    if(!$scope.course_id || $scope.course_id === '') { return; }
+	    if(!$scope.section_id || $scope.section_id === '') { return; }
+	    if(!$scope.semester || $scope.semester === '') { return; }
+	    if(!$scope.year || $scope.year === '') { return; }
+	    if(!$scope.course_name || $scope.course_name === '') { return; }
+	    sections.create({
+		course_id: $scope.course_id,
+		section_id: $scope.section_id,
+		semester: $scope.semester,
+		year: $scope.year,
+		course_name: $scope.course_name
+	    });
+	    $scope.course_id = '';
+	    $scope.section_id = '';
+	    $scope.semester = '';
+	    $scope.year = '';
+	    $scope.course_name = '';
+	};
+	$scope.addCards = function() {
+	    if (!$scope.card_title || $scope.card_title === '') {return;}
+	    if (!$scope.card_content || $scope.card_content === '') {return;}
+	    cards.create({
+		card_type: $scope.card_type,
+		title: $scope.card_title,
+		content: $scope.card_content,
+	    });
+	    $scope.card_type = '';
+	    $scope.card_title = '';
+	    $scope.card_content = '';
+	};
+	$scope.addTodo = function() {
+	    if (!$scope.todo_task || $scope.todo_task === '') { return; }
+	    todos.create({
+		task: $scope.todo_task
+	    });
+	    $scope.todo_task = '';
+	};
+	$scope.finishTask = function(todo) {
+	    todos.finish(todo);
+	};
+	$scope.undoTask = function(todo) {
+	    todos.undo(todo);
+	};
+    }]);
