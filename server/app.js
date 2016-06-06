c49b98a3665e8d252c339b8d91a325fce0eafeb1
이종윤 <jongyun.lee94@gmail.com>
2016-06-06 12:54:00 +0900

    mongoose setting changed

--- server/app.js
+++ server/app.js
@@ -17,7 +17,19 @@ var models = require('require-all')({
 });
 
 require('./config/passport');
-mongoose.connect('mongodb://localhost/cleannote');
+// mongoose.connect('mongodb://localhost/cleannote');
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/cleannote';
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+	console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+	console.log ('Succeeded connected to: ' + uristring);
+    }
+});
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
